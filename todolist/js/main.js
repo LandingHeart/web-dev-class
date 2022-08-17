33ab4aaec1ec67f2b20c0d51b10fbf4458b2cec6
LandingHeart <shinan.liu@yahoo.com>
2022-08-17 18:09:25 -0400

    save

--- todolist/js/main.js
+++
@@ -1,7 +0,0 @@
-function addItem() {
-  let inputVal = document.getElementById("input").value;
-  let ul = document.getElementById("ul-list-item");
-  let li = document.createElement("li");
-  li.appendChild(document.createTextNode(`${inputVal}`));
-  ul.appendChild(li);
-}
